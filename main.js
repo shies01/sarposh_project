3841384340d000439053364c1949e44aa0e70b3d
shies01 <shk.sakib.official@gmail.com>
2024-04-18 16:05:03 +0530

    improved the user expeince by utilising local storage

--- main.js
+++ main.js
@@ -1,94 +1,98 @@
-let form = document.querySelector("form");
-let name = document.getElementById("name");
-let  pax = document.getElementById("pax");
-let time = document.getElementById("time");
-let date = document.getElementById("date");
+let form = document.querySelector("#cant > form");
+let personName = document.getElementById("name");
+let paxNum = document.getElementById("pax");
+let expDate = document.getElementById("date");
+let expTime = document.getElementById("time");
 let mobileNum = document.getElementById("number");
-let table = document.getElementById("table");
-let tbody = document.querySelector("tbody");
-let allData = [];
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    let data = {
-      Cname : name.value,
-      Cpax : pax.value,
-      Ctime :time.value,
-      Cdate : date.value,
-      CMnum : mobileNum.value,
-      Ctable : table.value,
-    }
-    allData.push(data)
-    tbody.innerHTML = "";
-    allData.map((ele)=>{
-        let trow = document.createElement("tr");
-        let td1 = document.createElement("td");
-        let td2 = document.createElement("td");
-        let td3 = document.createElement("td");
-        let td4 = document.createElement("td");
-        let td5 = document.createElement("td");
-        let td6 = document.createElement("td");
-        let td7 = document.createElement("td");
-        let td8 = document.createElement("td");
-        let td9 = document.createElement("td");
-        let td10 = document.createElement("td");
-
-        td1.innerText = ele.Cdate;
-        td2.innerText = ele.Ctime;
-        td3.innerText = ele.Cname;
-        td4.innerText = ele.Cpax;
-        td5.innerText = ele.CMnum;
-        td6.innerText = ele.Ctable;
-        trow.append(td1,td2,td3,td4,td5,td6,td7,td8,td9,td10)
-        tbody.append(trow);
-
-        let status = document.createElement("h3")
-        td7.append(status)
-         
-        let arrive = document.createElement("button");
-        arrive.style.border = "none";
-        arrive.style.cursor = "pointer"
-        arrive.innerText = "ARRIVED";
-        td8.append(arrive)
-        arrive.addEventListener("click",function(e){
-            td7.innerText = "ARRIVED"
-            td7.style.background = "green"
-            td9.innerHTML = "";
-            td8.innerHTML = "";
-        })
-        let cancel = document.createElement("button");
-        cancel.style.border = "none";
-        cancel.style.cursor = "pointer"
-        cancel.innerText = "CANCELLED";
-        td9.append(cancel)
-        cancel.addEventListener("click",function(e){
-            td7.innerText = "CANCELLED"
-            td7.style.background = "red"
-            td8.innerHTML = "";
-            td9.innerHTML = "";
-        })
-
-        let del = document.createElement("button");
-        del.style.border = "none";
-        del.style.cursor = "pointer";
-        del.style.backgroundColor = "red"
-        del.innerText = "DELETE";
-        td10.append(del)
-        // del.addEventListener("click", function(e){
-        //     trow.innerHTML = "";
-        // })
-        // ... (Your existing code)
-
-       del.addEventListener("click", function(e) {
-    let index = allData.indexOf(ele);
-    allData.splice(index, 1);
-    trow.remove();
-});
-
-// ... (Your existing code)
-
-        td10.style.backgroundColor = "red"
-
-      
-    })
-
+let tableNum = document.getElementById("table")
+let tbodyContainer = document.querySelector('tbody')
+form.addEventListener("submit", function(){
+handlesubmit()
 })
+let AllData = []
+function handlesubmit(){
+    let data = {
+      name : personName.value,
+      pax : paxNum.value,
+      date : expDate.value,
+      time : expTime.value,
+      mobileNumber : mobileNum.value,
+      tableNumber : tableNum.value,
+    }
+AllData.push(data)
+saveData()
+}
+function saveData(){
+    localStorage.setItem("data", JSON.stringify(AllData))
+}
+function getData(){
+    let value = JSON.parse(localStorage.getItem("data"))
+    if(value){
+        AllData = value
+        showData(AllData)
+    }
+    
+}
+function showData(arr){
+    tbodyContainer.innerHTML=""
+  arr.forEach(function(ele,i){
+    let  tr = document.createElement("tr");
+    let td1 = document.createElement("td")
+    td1.innerHTML = ele.date;
+    let td2 = document.createElement("td");
+    td2.innerHTML = ele.time;
+    let td3 = document.createElement("td")
+    td3.innerHTML = ele.name;
+    let td4 = document.createElement("td")
+    td4.innerHTML = ele.pax;
+    let td5 = document.createElement("td");
+    td5.innerHTML = ele.mobileNumber;
+    let td6 = document.createElement("td")
+    td6.innerHTML = ele.tableNumber;
+    let td7 = document.createElement("td")
+    let yes = document.createElement("button")
+    yes.className = "yes_btn"
+    yes.addEventListener("click", function(){
+        td7.innerHTML = "Arrived"
+    })
+    yes.innerHTML = "Yes"
+    let no = document.createElement("button")
+    no.innerHTML = "No"
+    no.addEventListener("click", function(){
+        td7.innerHTML = "hasn't come"
+    })
+    no.className= "no_btn"
+    td7.append(yes,no)
+    let td8 = document.createElement("td")
+    let dlt = document.createElement("button")
+    dlt.innerHTML = "Delete";
+    dlt.className = "dlt"
+    dlt.addEventListener("click", function(){
+        handleDelete(i)
+    })
+    td8.append(dlt)
+    tr.append(td1,td2,td3,td4,td5,td6,td7,td8)
+    tbodyContainer.append(tr)
+  })
+
+}
+
+function handleDelete (index){
+    AllData.splice(index,1)
+        showData(AllData)
+        saveData()
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+getData()
